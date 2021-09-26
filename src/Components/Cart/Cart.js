f6c484a81e2f461b336f2620ae5f95731a0e211a
unknown <siamur01@gmail.com>
2021-09-26 21:33:39 +0600

    commit

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -5,16 +5,24 @@ import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import './Cart.css';
 
 const Cart = (props) => {
-   console.log(props.cart);
+   // console.log(props.cart);
    const { cart } = props;
    let total = 0;
    for (const teacher of cart) {
       total = total + teacher.salary;
    }
+
+   let addname = [];
+   for (const names of cart) {
+      let name = names.name;
+      addname = [...name, ", ", addname];
+
+   }
    return (
       <div className="cart">
          <h2>Teachers Added:{props.cart.length}</h2>
          <p><b> Total Cost:</b> $ {total}</p>
+         <p>names:{addname}</p>
          <div className="btn-cart-div">         <button className="cart-btn"> <FontAwesomeIcon icon={faInfoCircle} /> Teacher info</button></div>
       </div>
    );
